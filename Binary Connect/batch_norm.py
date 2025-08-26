48655e91b3d08931d66ca277c408083a62ec03c0
SHREY01022004 <134150008+SHREY01022004@users.noreply.github.com>
2025-08-26 10:16:50 +0530

    Binary Connect added

---
+++ Binary Connect/batch_norm.py
@@ -0,0 +1,79 @@
+import numpy as np
+import lasagne
+import theano
+import theano.tensor as T
+
+class BatchNormLayer(lasagne.layers.Layer):
+
+    def __init__(self, incoming, axes=None, epsilon=0.01, alpha=0.5,
+            nonlinearity=None, **kwargs):
+        
+        super(BatchNormLayer, self).__init__(incoming, **kwargs)
+        if axes is None:
+            # default: normalize over all but the second axis
+            axes = (0,) + tuple(range(2, len(self.input_shape)))
+        elif isinstance(axes, int):
+            axes = (axes,)
+        self.axes = axes
+        self.epsilon = epsilon
+        self.alpha = alpha
+        if nonlinearity is None:
+            nonlinearity = lasagne.nonlinearities.identity
+        self.nonlinearity = nonlinearity
+        shape = list(self.input_shape)
+        broadcast = [False] * len(shape)
+        for axis in self.axes:
+            shape[axis] = 1
+            broadcast[axis] = True
+        if any(size is None for size in shape):
+            raise ValueError("BatchNormLayer needs specified input sizes for "
+                             "all dimensions/axes not normalized over.")
+        dtype = theano.config.floatX
+        self.mean = self.add_param(lasagne.init.Constant(0), shape, 'mean',
+                                   trainable=False, regularizable=False)
+        self.std = self.add_param(lasagne.init.Constant(1), shape, 'std',
+                                  trainable=False, regularizable=False)
+        self.beta = self.add_param(lasagne.init.Constant(0), shape, 'beta',
+                                   trainable=True, regularizable=True)
+        self.gamma = self.add_param(lasagne.init.Constant(1), shape, 'gamma',
+                                    trainable=True, regularizable=False)
+
+    def get_output_for(self, input, deterministic=False, **kwargs):
+        if deterministic:
+            # use stored mean and std
+            mean = self.mean
+            std = self.std
+        else:
+            # use this batch's mean and std
+            mean = input.mean(self.axes, keepdims=True)
+            std = input.std(self.axes, keepdims=True)
+            # and update the stored mean and std:
+            # we create (memory-aliased) clones of the stored mean and std
+            running_mean = theano.clone(self.mean, share_inputs=False)
+            running_std = theano.clone(self.std, share_inputs=False)
+            # set a default update for them
+            running_mean.default_update = ((1 - self.alpha) * running_mean +
+                                           self.alpha * mean)
+            running_std.default_update = ((1 - self.alpha) * running_std +
+                                          self.alpha * std)
+            # and include them in the graph so their default updates will be
+            # applied (although the expressions will be optimized away later)
+            mean += 0 * running_mean
+            std += 0 * running_std
+        std += self.epsilon
+        mean = T.addbroadcast(mean, *self.axes)
+        std = T.addbroadcast(std, *self.axes)
+        beta = T.addbroadcast(self.beta, *self.axes)
+        gamma = T.addbroadcast(self.gamma, *self.axes)
+        normalized = (input - mean) * (gamma / std) + beta
+        return self.nonlinearity(normalized)
+
+def batch_norm(layer):
+
+    nonlinearity = getattr(layer, 'nonlinearity', None)
+    if nonlinearity is not None:
+        layer.nonlinearity = lasagne.nonlinearities.identity
+    if hasattr(layer, 'b'):
+        del layer.params[layer.b]
+        layer.b = None
+    return BatchNormLayer(layer, nonlinearity=nonlinearity)
